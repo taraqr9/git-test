a7a987733a2f958ed8e5d315a23a5fee8a858312
Taraq Rahman <taraqr9@gmail.com>
2025-08-09 12:26:25 +0600

    LookStatusEnum file updated

--- app/Enums/LookStatusEnum copy.php
+++ app/Enums/LookStatusEnum copy.php
@@ -7,4 +7,8 @@ enum LookStatusEnum: string
     case ACTIVE = 'active';
     case DELETED = 'deleted';
     case HIDDEN = 'hidden';
+    case TEST = 'test';
 }
+
+// this is nishus file
+// one more comment
